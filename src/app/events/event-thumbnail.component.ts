8d5059c71ab7d98a95ab58e9aa86dfe8ca5b6479
Teatoller <steven.otieno1@gmail.com>
2019-09-30 15:00:19 +0300

    (inter-component comms): Implement input to child component
    - add reference variable
    - add button with onclick event
    - add log function on thumbnail component
    - export
    
    [Starts #011]

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -1,5 +1,4 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
-// import { EventsListComponent } from './events-list.component'
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: "event-thumbnail",
@@ -14,16 +13,9 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
         <span>&nbsp;</span>
         <span>{{ event.location.city }}, {{ event.location.country }}</span>
       </div>
-      <button class="btn btn-primary" (click)="handleClickMe()">Click Me!</button>
     </div>
   `
 })
 export class EventThumbnailComponent {
   @Input() event: any;
-  @Output() eventClick = new EventEmitter()
-
-handleClickMe() {
-    this.eventClick.emit(this.event.name)
-    // console.log('clicked');
-}
 }
