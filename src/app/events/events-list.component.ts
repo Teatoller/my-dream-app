ed099eb01420c3b7ecd398bf3f2dd7d8ae595fa3
Teatoller <steven.otieno1@gmail.com>
2019-09-30 14:44:23 +0300

    (data):Implement child parent communication
    - add button on thumbnail with onclick event
    - add handleclick method
    - connect in event list component
    
    [Starts #010]

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,7 +6,8 @@ import { Component } from "@angular/core";
     <div>
       <h1>Upcoming Andela Events</h1>
       <hr />
-      <event-thumbnail [event]="event1"></event-thumbnail>
+      <event-thumbnail (eventClick)="handleEventClicked($event)" 
+      [event]="event1"></event-thumbnail>
     </div>
   `
 })
@@ -24,4 +25,8 @@ export class EventsListComponent {
       country: "Kenya"
     }
   };
+
+  handleEventClicked(data) {
+      console.log('received:', data)
+  }
 }
