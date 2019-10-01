f6037610ce4dcbdfb5784a6a74452f0e31ff036b
Teatoller <steven.otieno1@gmail.com>
2019-10-01 11:26:29 +0300

    (site header): Implement navbar
    - create nav component
    - add html file and link
    - add nav css style
    - add nav element in app component
    - declare and import nav component in module
    
    [Starts #013]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { EventsAppComponent } from "./events-app.component";
 import { EventsListComponent } from "./events/events-list.component";
 import { EventThumbnailComponent } from "./events/event-thumbnail.component";
+import { NavBarComponent } from "./nav/navbar.component";
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
-    EventThumbnailComponent
+    EventThumbnailComponent,
+    NavBarComponent
   ],
   imports: [BrowserModule, AppRoutingModule],
   bootstrap: [EventsAppComponent]
